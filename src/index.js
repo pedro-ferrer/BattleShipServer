785e12838647e218abd22841d7d42ffcd0a619c1
Pedro Ferrer <pedro.ferrer@santexgroup.com>
2019-02-12 18:28:34 -0300

    Adding subscriptions parameters

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
 const express = require('express');
-const graphqlHTTP = require ('express-graphql');
-const app = express();
+const {graphiqlExpress} = require('graphql-server-express');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-const _ = require('lodash');
+const cors = require('cors');
+const {createServer} = require('http');
+const {SubscriptionServer} = require('subscriptions-transport-ws');
+const { execute, subscribe } = require('graphql');
+const graphqlHTTP = require('express-graphql');
 
 mongoose.connect('mongodb+srv://root:root@cluster0-awigs.mongodb.net/test?retryWrites=true');
-mongoose.connection.once('open',()=>{
+mongoose.connection.once('open', () => {
     console.log('Connected to database.');
 })
-app.use('/graphql', graphqlHTTP({
-   schema: schema,
-   graphiql: true
+
+const PORT = 4000;
+const server = express();
+
+server.use(cors());
+
+server.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  }));
+
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+  subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
 }));
 
-app.listen(4000, () => {
-    console.log('now listening for requests on port 4000')
+// Wrap the Express server
+const ws = createServer(server);
+ws.listen(PORT, () => {
+  console.log(`Apollo Server is now running on http://localhost:${PORT}`);
+  // Set up the WebSocket for handling GraphQL subscriptions
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema
+  }, {
+    server: ws,
+    path: '/subscriptions',
+  });
 });
